6a9b7cbba97da2b88bdd420e7524982347dbe798
jmc0504 <sckyu12345@gmail.com>
2021-03-09 09:01:01 +0000

    1931

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int N,result=0;
+
+
+bool a(pair<int,int>b,pair<int,int>c){
+  if(b.second==c.second) return b.first<c.first;
+  return b.second<c.second;
+}
+
+int main(){
+  cin>>N;
+  vector<pair<long long,long long>>container(N);
+  for(int i=0; i<N; i++){
+    cin>>container[i].first>>container[i].second;
+  }
+  sort(container.begin(),container.end(),a);
+
+  int first=-1,second=-1,num=0;
+  for(int i=0; i<N; i++){
+    if(container[i].first>=second) {
+      first = container[i].first;
+      second = container[i].second;
+      result++;
+    }
+    if(first == container[N-1].first && second == container[N-1].second) break;
+  }
+  cout<<result;
 }
