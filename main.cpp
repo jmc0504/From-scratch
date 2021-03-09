53525e7f3610a5bd82c03463e29dd4bec6127ffa
jmc0504 <sckyu12345@gmail.com>
2021-03-09 09:02:25 +0000

    11047

--- main.cpp
+++ main.cpp
@@ -1,32 +1,20 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
 
-int N,result=0;
-
-
-bool a(pair<int,int>b,pair<int,int>c){
-  if(b.second==c.second) return b.first<c.first;
-  return b.second<c.second;
-}
-
+int N,K,result=0;
 int main(){
-  cin>>N;
-  vector<pair<long long,long long>>container(N);
-  for(int i=0; i<N; i++){
-    cin>>container[i].first>>container[i].second;
-  }
-  sort(container.begin(),container.end(),a);
+  cin>>N>>K;
+  int* lis = new int[N];
 
-  int first=-1,second=-1,num=0;
   for(int i=0; i<N; i++){
-    if(container[i].first>=second) {
-      first = container[i].first;
-      second = container[i].second;
-      result++;
-    }
-    if(first == container[N-1].first && second == container[N-1].second) break;
+    cin>>lis[i];
   }
+
+  for(int i = N-1; i>-1; i--){
+    if(K==0) break;
+    result+=K/lis[i];
+    K-=lis[i]*(K/lis[i]);
+  }
+
   cout<<result;
 }
