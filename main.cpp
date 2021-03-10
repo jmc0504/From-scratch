09c29f8a97f0a4925b2722ec5e2567cbef36f7fa
jmc0504 <sckyu12345@gmail.com>
2021-03-10 14:14:00 +0000

    2231

--- main.cpp
+++ main.cpp
@@ -3,22 +3,27 @@
 #include<algorithm>
 using namespace std;
 
-int n,m;
+int n,buffer;
 int result=0;
-vector<int>v;
-int main(){
-  cin>>n>>m;
-  for(int i=0; i<n; i++){
-    int num;
-    cin>>num;
-    v.push_back(num);
-  }
 
-  for(int i=0; i<n; i++){
-    for(int j=i+1; j<n; j++){
-      for(int k=j+1; k<n; k++){
-        if(v[i]+v[j]+v[k]<=m && m-result>m-(v[i]+v[j]+v[k])) result = v[i]+v[j]+v[k];
-      }
+int disintegrate(int a){
+  int result=a, b=1;
+  while(b*10<a) b*=10;
+  while(b>0){
+    result+=a/b;
+    a-=b*(a/b);
+    b/=10;
+  }
+  return result;
+}
+
+
+int main(){
+  cin>>n;
+  for(int i=1; i<n; i++){
+    if(disintegrate(i)==n){
+      result = i;
+      break;
     }
   }
   cout<<result;
