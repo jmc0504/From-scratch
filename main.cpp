770fa5a885ee53f4ef6c92fe0263ecf57eb2f3f4
jmc0504 <sckyu12345@gmail.com>
2021-03-10 13:37:57 +0000

    2798

--- main.cpp
+++ main.cpp
@@ -3,26 +3,23 @@
 #include<algorithm>
 using namespace std;
 
-long long n,price=0;
-vector<long long>road;
-vector<long long>oil;
-
+int n,m;
+int result=0;
+vector<int>v;
 int main(){
-  cin>>n;
-  for(int i=0; i<n-1; i++){
-    int o;
-    cin>>o;
-    road.push_back(o);
-  }
+  cin>>n>>m;
   for(int i=0; i<n; i++){
-    int o;
-    cin>>o;
-    oil.push_back(o);
+    int num;
+    cin>>num;
+    v.push_back(num);
   }
-  long long oil_price=1000000001,length;
+
   for(int i=0; i<n; i++){
-    if(oil_price>oil[i]) oil_price = oil[i];
-    price+=oil_price*road[i];
+    for(int j=i+1; j<n; j++){
+      for(int k=j+1; k<n; k++){
+        if(v[i]+v[j]+v[k]<=m && m-result>m-(v[i]+v[j]+v[k])) result = v[i]+v[j]+v[k];
+      }
+    }
   }
-  cout<<price;
+  cout<<result;
 }
