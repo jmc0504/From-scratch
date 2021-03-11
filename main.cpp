110ed6a5a90e5a5032a49fe84f78978b2ffffcac
jmc0504 <sckyu12345@gmail.com>
2021-03-11 14:05:18 +0000

    1018

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
 
-int n,x,y;
-
-
+int n,m,result = 99999;
 
 int main(){
-  cin>>n;
-  vector<pair<int,int>>size(n);
-  vector<int>result(n,1);
-  for(int i=0; i<n; i++){
-    cin>>size[i].first>>size[i].second;
+  cin>>n>>m;
+  vector<vector<char>>chess(n,vector<char>(m));
+
+  for(auto& row : chess){
+    for(auto& col : row){
+      cin>>col;
+    }
   }
   
-  for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-      if(size[i].first<size[j].first && size[i].second<size[j].second && i!=j) result[i]++;
+  int b_result,w_result;
+  for(int col_point=0; col_point<m-7; col_point++){
+    for(int row_point=0; row_point<n-7; row_point++){
+      b_result=0,w_result=0;
+      for(int row = row_point; row<row_point+8; row++){
+        for(int col = col_point; col<col_point+8; col++){
+          if ((row + col) % 2 == 0) {
+						if (chess[row][col] == 'W') w_result++;
+						else b_result++;
+					}
+					else {
+						if (chess[row][col] == 'W') b_result++;
+						else w_result++;
+					}
+        }
+      }
+      if(result>b_result) result = b_result;
+      if(result>w_result) result = w_result;
     }
-    cout<<result[i]<<" ";
   }
-
- 
+  cout<<result;
 }
