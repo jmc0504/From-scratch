b4e6874a54f27ed9070ab75eabc6692845c78623
jmc0504 <sckyu12345@gmail.com>
2021-03-18 13:49:18 +0000

    10773

--- main.cpp
+++ main.cpp
@@ -1,47 +1,24 @@
 #include<iostream>
-#include<vector>
+#include<stack>
 using namespace std;
 
-/*
-push X: 정수 X를 스택에 넣는 연산이다.
-pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-size: 스택에 들어있는 정수의 개수를 출력한다.
-empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
-top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-*/
-
-
-int n;
-vector<int>v;
-string x;
+int n,result=0;
+stack<int>v;
 
 int main(){
   cin>>n;
-  
-  while(n){
-    cin>>x;
-    if(x.find("push")!=-1){
-      cin>>x;
-      int k = stoi(x);
-      v.push_back(k);
-    }
-    else if(x.find("pop")!=-1){
-      if(v.size()!=0){
-        cout<<v[v.size()-1]<<endl;
-        v.pop_back();
-      }
-      else cout<<-1<<endl;
-    }
-    else if(x.find("size")!=-1){
-      cout<<v.size()<<endl;
-    }
-    else if(x.find("empty")!=-1){
-      cout<<v.empty()<<endl;
-    }
-    else if(x.find("top")!=-1){
-      if(v.size()!=0) cout<<v[v.size()-1]<<endl;
-      else cout<<-1<<endl;
-    }
-    n--;
+
+  for(int i=0; i<n; i++){
+    int l;
+    cin>>l;
+
+    if(l!=0){v.push(l);}
+    else{v.pop();}
   }
+  
+  for(int i=v.size(); i>0; i--){
+    result+=v.top();
+    v.pop();
+  }
+  cout<<result;
 }
